800030578e6c6aef2628b2304827f3818f5b7083
Lee_JunWon <cpprhtn@naver.com>
2020-06-02 23:07:09 +0900

    6/2

--- 코로나/corona_distribute_Kor.R
+++ 코로나/corona_distribute_Kor.R
@@ -77,7 +77,7 @@ plot(x=Kor_Data$time, y=Kor_Data$cum_confirm)
 p <- ggplot(Kor_Data, aes(time, cum_confirm))
 print(p)
 #종합 2/20 ~ 5/29
-plot(x=dd$time,y=dd$confirm,type = "o", col = "red", main = "Kor_increasing_trend",xlim = c(min(as.Date(dd$time)),max(as.Date(dd$time))),
+plot(x=kor_coda$time,y=kor_coda$confirm,type = "o", col = "red", main = "Kor_increasing_trend",xlim = c(min(as.Date(dd$time)),max(as.Date(dd$time))),
      xlab = "Date", ylab = "confirm",lty =6, sub = "red = confirm, blue = heal, black = dead")
 lines(dd$time,dd$cum_heal,col="blue")
 points(dd$time,dd$cum_heal,col="blue")
@@ -145,5 +145,56 @@ kor_coda <- rbind(kk,newd)
 write.csv(kor_coda, "/Users/cpprhtn/Desktop/Kor_coda.csv")
 
 
+update_coda <- dd[103,c(1,3,4,5)]
+kor_coda <- rbind(kor_coda,update_coda)
 
+fit <- lm(kor_coda$confirm ~ kor_coda$time)
+summary(fit)
+abline(fit, col = "purple",  lwd="3")
+fit$coefficients[[1]]
+
+#전체 기울기
+fit$coefficients[[2]] #109.5699
+
+#신천지 이전
+coda1 <- kor_coda[c(1:28),]
+fit_1 <- lm(coda1$confirm ~ coda1$time)
+fit_1$coefficients[[2]] #0.2208539
+abline(fit_1, col="dark blue", lwd="3")
+
+#신천지 영항력 2/18 ~ 3/8
+coda2 <- kor_coda[c(29:48),]
+fit_2 <- lm(coda2$confirm ~ coda2$time)
+fit_2$coefficients[[2]] #432.2105
+abline(fit_2,col="black", lwd="3")
+
+#구로 콜센터 3/9~3/25
+coda3 <- kor_coda[c(49:65),]
+fit_3 <- lm(coda3$confirm ~ coda3$time)
+fit_3$coefficients[[2]] #103.6201
+abline(fit_3,col="dark blue", lwd="3")
+
+#사이 4/10~5/9
+coda4 <- kor_coda[c(81:110),]
+fit_4 <- lm(coda4$confirm ~ coda4$time)
+fit_4$coefficients[[2]] #24.12121
+abline(fit_4,col="green", lwd="3")
+
+#이태원 5/10~5/15
+coda5 <- kor_coda[c(111:116),]
+fit_5 <- lm(coda5$confirm ~ coda5$time)
+fit_5$coefficients[[2]] #28.34286
+abline(fit_5,col="yellow", lwd="3")
+
+#사이 5/16~5/25
+coda6 <- kor_coda[c(117:126),]
+fit_6 <- lm(coda6$confirm ~ coda6$time)
+fit_6$coefficients[[2]] #19.42424
+abline(fit_6,col="green", lwd="3")
+
+#쿠팡 집단감염 5/27 ~
+coda7 <- kor_coda[c(127:133),]
+fit_7 <- lm(coda7$confirm ~ coda7$time)
+fit_7$coefficients[[2]] #47.75
+abline(fit_7,col="green", lwd="3")
 
